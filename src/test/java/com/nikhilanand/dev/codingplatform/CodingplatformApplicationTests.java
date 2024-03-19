8d955aaf8e5387438dead1f3870d64a0d987dcf2
nikhilananddev <nikhilanand1672000@gmail.com>
2024-03-19 15:11:51 +0530

    All dependency is added

---
+++ src/test/java/com/nikhilanand/dev/codingplatform/CodingplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikhilanand.dev.codingplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
